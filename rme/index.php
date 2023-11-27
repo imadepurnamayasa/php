421e3b2c140f3b078d6108a6f09f3ccc3f2c9261
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2023-11-27 10:56:21 +0800

    update

---
+++ rme/index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('ROOT', __DIR__);
+define('FOLDER_CLASS', ROOT . '/' . 'class/');
+define('FOLDER_CONFIG', ROOT . '/' . 'config/');
+define('FOLDER_FUNCTION', ROOT . '/' . 'function/');
+define('FOLDER_INCLUDE', ROOT . '/' . 'include/');
+define('FOLDER_MODULE', ROOT . '/' . 'module/');
+define('FOLDER_PUBLIC', ROOT . '/' . 'public/');
